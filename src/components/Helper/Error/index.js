5cb08be741a2a39dab28966deff204c2890c3590
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-13 05:40:31 -0300

    code idented

--- src/components/Helper/Error/index.js
+++ src/components/Helper/Error/index.js
@@ -4,12 +4,7 @@ const Error = ({ error }) => {
   if (!error) return null;
 
   return (
-    <p 
-      style={{ 
-        color: '#F31', 
-        margin: '1rem 0' 
-      }}
-    >
+    <p style={{ color: '#F31', margin: '1rem 0' }}>
       {error}
     </p>
   );
