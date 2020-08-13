7662bedfc052af933e1aec921cec72a887ef16a4
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-13 02:53:02 -0300

    fix key error on photo-comments component

--- src/components/Photo/PhotoComments/index.js
+++ src/components/Photo/PhotoComments/index.js
@@ -27,7 +27,7 @@ const PhotoComments = (props) => {
     <>
       <ul ref={commentsSection} className={styles.comments}>
         {comments.map(comment => (
-          <li key={comment._ID}>
+          <li key={comment.comment_ID}>
             <b>{comment.comment_author}: </b>
             <span>{comment.comment_content}</span>
           </li>
