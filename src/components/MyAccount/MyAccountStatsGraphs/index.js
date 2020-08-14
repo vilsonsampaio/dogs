e1b531d6c09fdb333b00578f1dafcf232942a701
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-14 00:23:03 -0300

    victory lib (graphs)  added and configured

---
+++ src/components/MyAccount/MyAccountStatsGraphs/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
+
+import styles from './styles.module.css';
+
+const MyAccountStatsGraphs = ({ data }) => {
+  const [graph, setGraph] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
+  console.log(data)
+  React.useEffect(() => {
+    const accessArray = data.map(data => Number(data.acessos));
+    const accessSum = accessArray.reduce((acc, cur) => acc + cur);
+
+    setTotal(accessSum);
+
+    const graphDataArray = data.map(data => {
+      return {
+        x: data.title,
+        y: Number(data.acessos),
+      }
+    });
+
+    setGraph(graphDataArray);
+  }, [data]);
+
+  return (
+    <section className={`animeLeft ${styles.graph}`}>
+
+      <div className={`${styles.total} ${styles.graphItem}`}>
+        <p>Acessos: {total}</p>
+      </div>
+
+
+      <div className={styles.graphItem}>
+        <VictoryPie 
+          data={graph}
+          innerRadius={50}
+          padding={{
+            top: 20,
+            bottom: 20,
+            left: 80,
+            right: 80,
+          }}
+          style={{
+            data: {
+              fillOpacity: 0.9,
+              stroke: '#FFF',
+              strokeWidth: 2,
+            },
+            labels: {
+              fontSize: 14,
+              fill: '#333',
+            },
+          }} 
+        />
+      </div>
+
+      <div className={styles.graphItem}>
+        <VictoryChart>
+          <VictoryBar data={graph} alignment="start" />
+        </VictoryChart>
+      </div>
+    </section>
+  );
+}
+
+export default MyAccountStatsGraphs;
--- src/pages/MyAccount/MyAccountStats/index.js
+++ src/pages/MyAccount/MyAccountStats/index.js
@@ -1,16 +1,46 @@
 import React from 'react';
 
-import Head from '../../../components/Helper/Head';
+import useFetch from '../../../hooks/useFetch';
 
+import Head from '../../../components/Helper/Head';
+import Loading from '../../../components/Helper/Loading';
+import Error from '../../../components/Helper/Error';
+
+import MyAccountStatsGraphs from '../../../components/MyAccount/MyAccountStatsGraphs';
+
+
+import { STATS_GET } from '../../../services/api';
 
 const MyAccountStatics = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
+  const { request, data, loading, error } = useFetch();
 
-      My Account Stats
-    </div>
-  );
+  React.useEffect(() => {
+    const token = window.localStorage.getItem('DOGS_token');
+
+    async function getData() {
+      const { url, options } = STATS_GET(token);
+      
+      await request(url, options);
+    }
+
+    if (token) {
+      getData();
+    }
+  }, [request]);
+
+  if (loading) return <Loading />
+
+  if (error) return <Error error={error} />
+
+  if (data) 
+    return (
+      <div>
+        <Head title="Estatísticas" />
+
+        <MyAccountStatsGraphs data={data} />
+      </div>
+    );
+  else return null; 
 }
 
 export default MyAccountStatics;
